273b87c645947c998032e21368bdb813d5579adc
Peter Hessler <peterhessler@hotmail.co.uk>
2022-02-21 10:23:48 +0000

    Basic Form added

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template("index.html")
+app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
 
-if __name__ == "__main__":
-    app.run(debug=False)
+class SongInfo(FlaskForm):
+    artist = StringField('Artist')
+    title = StringField('Track Title')
+    submit = SubmitField('Add Name')
+
+@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/home', methods=['GET', 'POST'])
+def register():
+    message = ""
+    form = SongInfo()
+
+    if request.method == 'POST':
+        artist = form.artist.data
+        title = form.title.data
+
+        if len(artist) == 0 or len(title) == 0:
+            message = "Please supply both artist and title"
+        else:
+            message = f'Wow, {title} by {artist} is a Banger'
+
+    return render_template('home.html', form=form, message=message)
+
+if __name__ == '__main__':
+     app.run(debug=False, host='0.0.0.0')
  
